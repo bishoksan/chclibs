2bc858b50798ced065bb7dede746922e382d223f
Jose F. Morales <jfmcjf@gmail.com>
2016-03-05 12:27:55 +0100

    Update dependencies

--- Manifest/Manifest.pl
+++ Manifest/Manifest.pl
@@ -1,7 +1,11 @@
 % Manifest file for CHCLibs
 bundle_name(chclibs).
 bundle_packname('CHCLibs').
-bundle_requires([core]).
+bundle_requires([
+    core,
+    % ciao_ppl, % TODO: still in Ciao
+    ciao_yices
+]).
 bundle_alias_paths([
     chclibs = 'src'
 ]).
