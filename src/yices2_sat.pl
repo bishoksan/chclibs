4aeb3f4f127d9a52840ef04633b6d70981a9d155
Jose F. Morales <jfmcjf@gmail.com>
2016-04-15 11:15:53 +0200

    Using a DCG for expr2yices translation (linear complexity)

--- src/yices2_sat.pl
+++ src/yices2_sat.pl
@@ -6,41 +6,36 @@
 	true_in_model/2,
 	get_value_as_term/3,
 	yices_vars/3
-   ], [assertions, isomodes, doccomments]).
+   ], [assertions, isomodes, doccomments, dcg]).
 
-:- use_module(library(write)).
+%! \title Higher level interface to Yices SMT solver
+
+:- use_module(library(format)).
 :- use_module(library(lists)).
 :- use_module(library(strings)).
 
 :- use_module(ciao_yices(ciao_yices_2)).
 
+% E: a yices expression (see expr2yices/2)
+% Vars: list of (V,Tau) where Tau is a yices type
 yices_sat(E,Vars) :-
-	expr2yices(E,Y),
-	!,
-	%write_string(Y),nl,
-	declareVars(Vars),
-	yices_context(Ctx),
-	yices_parse_term(Y,T),
-	reportError(T),
-	yices_assert_formula(Ctx,T,_Status),
-	yices_check(Ctx,StatusName),
-	%write(StatusName),nl,
-	StatusName==satisfiable,
-	yices_free_context(Ctx).
+	yices_check_formula(E, Vars, satisfiable).
 	
+% E: a yices expression (see expr2yices/2)
+% Vars: list of (V,Tau) where Tau is a yices type
 yices_unsat(E,Vars) :-
+	yices_check_formula(E, Vars, unsatisfiable).
+
+yices_check_formula(E,Vars,StatusName) :-
 	expr2yices(E,Y),
-	!,
-	%write('SMT formula: '), nl,
-	%write_string(Y),nl,
 	declareVars(Vars),
 	yices_context(Ctx),
 	yices_parse_term(Y,T),
+	check_no_error(T),
 	yices_assert_formula(Ctx,T,_Status),
-	yices_check(Ctx,StatusName),
-	%write(StatusName),nl,
-	StatusName==unsatisfiable,
-	yices_free_context(Ctx).
+	yices_check(Ctx,StatusName0),
+	yices_free_context(Ctx),
+	StatusName = StatusName0.
 
 % (do not include eliminated variables)
 yices_model(E,Vars,M) :-
@@ -52,13 +47,8 @@ yices_model_keepsubst(E,Vars,M) :-
 
 yices_model_(E,Vars,KeepSubst, M) :-
 	expr2yices(E,Y),
-	%write('clp formula '), nl,
-	%write(E), nl,
-	!,
-	%write('SMT formula: '), nl,
-	%write_string(Y),nl,
 	declareVars(Vars),
-	yices_context(Ctx),
+	yices_context(Ctx), % TODO: Ctx leaked?
 	yices_parse_term(Y,T),
 	yices_assert_formula(Ctx,T,_Status),
 	yices_check(Ctx,StatusName),
@@ -69,122 +59,83 @@ yices_model_(E,Vars,KeepSubst, M) :-
 	
 true_in_model(E,M) :-
 	expr2yices(E,Y),
-	!,
 	yices_parse_term(Y,T),
 	yices_formula_true_in_model(M,T,Status),
 	Status==1.
 
 get_value_as_term(M, E, Term) :-
 	expr2yices(E,Y),
-	!,
 	yices_parse_term(Y,T),
 	yices_get_value_as_term(M,T,Term).
 
+expr2yices(E,S) :-
+	exp_(E,S,[]).
+%	format(user_error, "[~w: ~s]~n", [E,S]).
 	
-expr2yices([X1,X2|Y],S) :-
-	expr2yices(X1,S1),
-	expr2yices([X2|Y],S2),
-	concatStrings(["(","and ",S1," ",S2,")"],S).
-expr2yices([false],"false").
-expr2yices([X],S) :-
-	expr2yices(X,S).
-expr2yices([],"true").
-expr2yices((X;Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","or ",S1," ",S2,")"],S).
-expr2yices((X,Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","and ",S1," ",S2,")"],S).
-expr2yices((X<Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","< ",S1," ",S2,")"],S).
-expr2yices((X>Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","> ",S1," ",S2,")"],S).
-expr2yices((X=<Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","<= ",S1," ",S2,")"],S).
-expr2yices((X>=Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(",">= ",S1," ",S2,")"],S).
-expr2yices((X*Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","* ",S1," ",S2,")"],S).
-expr2yices((X+Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","+ ",S1," ",S2,")"],S).
-expr2yices((X-Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","- ",S1," ",S2,")"],S).
-expr2yices(-(X),S) :-
-	expr2yices(X,S1),
-	concatStrings(["(","- ",S1,")"],S).
-expr2yices((X=Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","= ",S1," ",S2,")"],S).
-expr2yices(xor(X,Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","xor ",S1," ",S2,")"],S).
-expr2yices((X->Y),S) :-
-	expr2yices(X,S1),
-	expr2yices(Y,S2),
-	concatStrings(["(","=> ",S1," ",S2,")"],S).
-expr2yices(neg(X),S) :-
-	expr2yices(X,S1),
-	concatStrings(["(","not ",S1,")"],S).
-expr2yices('$VAR'(N),S) :-
-	name(N,I),
-	append("x",I,S).
-expr2yices(A,S) :-
-	atomic(A),
-	name(A,S).
-expr2yices(A,"") :-
-	write('problem with '),
-	write(A),
-	nl.
-	
+exp_([X1,X2|Y]) --> !,
+	"(","and ",exp_(X1)," ",exp_([X2|Y]),")".
+exp_([false]) --> !, "false".
+exp_([X]) --> !, exp_(X).
+exp_([]) --> !, "true".
+exp_((X;Y)) --> !, "(","or ",exp_(X)," ",exp_(Y),")".
+exp_((X,Y)) --> !,
+	"(","and ",exp_(X)," ",exp_(Y),")".
+exp_((X<Y)) --> !,
+	"(","< ",exp_(X)," ",exp_(Y),")".
+exp_((X>Y)) --> !,
+	"(","> ",exp_(X)," ",exp_(Y),")".
+exp_((X=<Y)) --> !,
+	"(","<= ",exp_(X)," ",exp_(Y),")".
+exp_((X>=Y)) --> !,
+	"(",">= ",exp_(X)," ",exp_(Y),")".
+exp_((X*Y)) --> !,
+	"(","* ",exp_(X)," ",exp_(Y),")".
+exp_((X+Y)) --> !,
+	"(","+ ",exp_(X)," ",exp_(Y),")".
+exp_((X-Y)) --> !,
+	"(","- ",exp_(X)," ",exp_(Y),")".
+exp_(-(X)) --> !,
+	"(","- ",exp_(X),")".
+exp_((X=Y)) --> !,
+	"(","= ",exp_(X)," ",exp_(Y),")".
+exp_(xor(X,Y)) --> !,
+	"(","xor ",exp_(X)," ",exp_(Y),")".
+exp_((X->Y)) --> !, % TODO: what is this?
+	"(","=> ",exp_(X)," ",exp_(Y),")".
+exp_(neg(X)) --> !,
+	"(","not ",exp_(X),")".
+exp_('$VAR'(N)) --> !,
+	{ name(N,I) },
+	str("x"||I).
+exp_(A) --> { atomic(A) }, !,
+	{ name(A,S) },
+	str(S).
+exp_(A) -->
+	{ throw(error(wrong_expr(A), expr2yices/2)) }.
 
-declareVars([(X,real)|Vars]) :-
+str(X,S,S0) :- append(X,S0,S).
+
+
+declareVars([(X,Tau)|Vars]) :- !,
 	expr2yices(X,V),
-	!,
-	yices_declare_real(V),
-	declareVars(Vars).
-declareVars([(X,int)|Vars]) :-
-	expr2yices(X,V),
-	!,
-	yices_declare_int(V),
-	declareVars(Vars).
-declareVars([(X,bool)|Vars]) :-
-	expr2yices(X,V),
-	!,
-	yices_declare_bool(V),
+	yices_declare_var(Tau,V),
 	declareVars(Vars).
 declareVars([]).
 
+yices_declare_var(real, V) :- yices_declare_real(V).
+yices_declare_var(int, V) :- yices_declare_int(V).
+yices_declare_var(bool, V) :- yices_declare_bool(V).
+	
 
-concatStrings([],"").
-concatStrings([S|Ss],R) :-
-	concatStrings(Ss,R1),
-	append(S,R1,R).
+check_no_error(S) :-
+	( S<0 -> report_error
+	; true
+	).
 	
-reportError(S) :-
-	S<0 -> reportErrorState; true.
-	
-reportErrorState :-
+report_error :-
 	yices_error_string(E),
-	write_string(E),
-	nl.
+	throw(yices_error(E)).
+
 
 yices_vars([], _Type, []).
 yices_vars([V|Vs], Type, [(V,Type)|VTs]):-
